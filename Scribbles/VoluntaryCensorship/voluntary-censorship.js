2632d19f5b578a93cedad3ff12e3733b28617108
Tsuyoshi Iguchi <tsuyoshi.iguchi@gmail.com>
2021-06-24 21:21:24 +0900

    seach by classes to textContent of h2

--- Scribbles/VoluntaryCensorship/voluntary-censorship.js
+++ Scribbles/VoluntaryCensorship/voluntary-censorship.js
@@ -1,20 +1,22 @@
 const timeout = 2000
 const useTimer = true
 
-function classesToSelector(classes) {
-    return "." + classes.replaceAll(" ", ".");
-}
-function hideBySelector(selector)
-{
-    const nodes = document.querySelectorAll(selector)
-    for(var i = 0; i < nodes.length; i++)
-    {
-	const node = nodes[i]
-	node.style.display = "none"
-    }
+const titles = new Set(["What’s happening", "Topics to follow"])
+
+function hideNode(node) {
+    node.style.display = "none"
 }
 function process() {
-    hideBySelector(classesToSelector("css-1dbjc4n r-1ihkh82 r-1in3vh1 r-1867qdf r-1phboty r-rs99b7 r-1ifxtd0 r-1udh08x"))
+    const nodes = document.querySelectorAll("h2")
+    for(var i = 0; i < nodes.length; i++)
+    {
+        const node = nodes[i]
+        if(titles.has(node.textContent))
+        {
+            hideNode(node.parentNode.parentNode.parentNode)
+            node.style.visibility
+        }        
+    }
 }
 function callback() {
     process()
@@ -28,7 +30,3 @@ document.addEventListener("DOMContentLoaded", callback)
 console.log("start Voluntary Censorship")
 
 callback()
-
-
-
-
