e4d04f76c6b8b6f0898876b1074d2cd7588421b8
Tsuyoshi Iguchi <tsuyoshi.iguchi@gmail.com>
2021-09-19 23:14:42 +0900

    add avdevice_register_all() call.

--- Experiment/ffmpeg-v4l2/testv4l2.c
+++ Experiment/ffmpeg-v4l2/testv4l2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <libavformat/avformat.h>
 #include <libavcodec/avcodec.h>
-
+#include <libavdevice/avdevice.h>
 
 int main(void)
 {
@@ -9,18 +9,22 @@ int main(void)
 
     av_log_set_level(AV_LOG_TRACE);
     av_log_set_flags(AV_LOG_PRINT_LEVEL);
+
+    avdevice_register_all();
+
     AVDictionary *opts = NULL;
     AVFormatContext* format =  avformat_alloc_context();
     format->interrupt_callback.callback = NULL;
     format->interrupt_callback.opaque = NULL;
 
-    AVInputFormat* inputFormat = av_find_input_format("video4linux2");
+    AVInputFormat* inputFormat = av_find_input_format("v4l");
     printf("inputFormat: %p\n", inputFormat);
-
+    inputFormat = NULL;
     //av_dict_set(&opts, "truncate", "0", 0);
     //av_dict_set(&opts, "seekable", "0", 0);
     av_dict_set(&opts, "video_size", "640x480", 0);
-    av_dict_set(&opts, "input_format", "YUYV", 0);
+    //av_dict_set(&opts, "input_format", "YUYV", 0);
+    //av_dict_set(&opts, "use_libv4l2", "1", 0);
 
     ret = avformat_open_input(&format, "/dev/video0", inputFormat, &opts);
     if (ret != 0)
